b25c23bbcdc55bf19813d0b7a0c72d72daf5c228
Tiago Catoia <tiagocsz@hotmail.com>
2025-08-04 16:54:24 -0300

    feat: add User entity model

---
+++ MarketLinker.Domain/Entities/User/User.cs
@@ -0,0 +1,12 @@
+﻿using MarketLinker.Domain.Entities.Marketplace.Auth;
+
+namespace MarketLinker.Domain.Entities.User;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+
+    public ICollection<MarketplaceAuthBase> MarketplaceAuths { get; set; } = new List<MarketplaceAuthBase>();
+}
