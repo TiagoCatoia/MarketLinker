c5858ed970f9bab505345948f144108fd2001ac7
Tiago Catoia <tiagocsz@hotmail.com>
2025-08-05 10:26:58 -0300

    feat: setup environment variables and in-memory database

--- MarketLinker.Api/Program.cs
+++ MarketLinker.Api/Program.cs
@@ -1,6 +1,14 @@
+using MarketLinker.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+DotNetEnv.Env.Load();
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.AddDbContext<MarketLinkerDbContext>(opt => opt.UseInMemoryDatabase("PersonDb"));
 
 if (app.Environment.IsDevelopment())
 {
