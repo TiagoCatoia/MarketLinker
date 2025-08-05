a17862553ad95aa5ce99c9cb632ec144a2f9218d
Tiago Catoia <tiagocsz@hotmail.com>
2025-08-05 16:02:40 -0300

    feat: add RefreshToken entity

---
+++ MarketLinker.Domain/Entities/Marketplace/Auth/RefreshToken.cs
@@ -0,0 +1,14 @@
+﻿using UserEntity = MarketLinker.Domain.Entities.User.User;
+
+namespace MarketLinker.Domain.Entities.Marketplace.Auth;
+
+public class RefreshToken
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public string Token { get; set; } = null!;
+    public DateTime ExpiryDate { get; set; }
+    public bool IsRevoked { get; set; }
+    
+    public Guid UserId { get; set; }
+    public UserEntity User { get; set; } = null!;
+}
