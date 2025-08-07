5643daefc3159185bd998632a7058d3b8f756d05
Tiago Catoia <tiagocsz@hotmail.com>
2025-08-07 18:09:02 -0300

    feat: add user entity to DTO mapping

---
+++ MarketLinker.Application/Mappings/UserMapper.cs
@@ -0,0 +1,18 @@
+﻿using MarketLinker.Application.DTOs.User;
+using MarketLinker.Domain.Entities.User;
+
+namespace MarketLinker.Application.Mappings;
+
+public static class UserMapper
+{
+    public static UserDto ToDto(User user)
+    {
+        return new UserDto
+        {
+            Id = user.Id,
+            Email = user.Email
+        };
+    }
+    
+    
+}
