8da7dbdf0b44a38eda63b844277ac3ad97375089
Tiago Catoia <tiagocsz@hotmail.com>
2025-08-11 09:06:07 -0300

    feat: add MercadoLivreTokenResponse DTO for Mercado Livre OAuth token data

---
+++ MarketLinker.Application/DTOs/Marketplace/MercadoLivre/MercadoLivreTokenResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MarketLinker.Application.DTOs.Marketplace.MercadoLivre;
+
+public class MercadoLivreTokenResponse
+{
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; init; } = null!;
+
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; init; } = null!;
+
+    [JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; init; }
+
+    [JsonPropertyName("scope")]
+    public string Scope { get; init; } = null!;
+
+    [JsonPropertyName("user_id")]
+    public long UserId { get; init; }
+
+    [JsonPropertyName("refresh_token")]
+    public string RefreshToken { get; init; } = null!;
+}
