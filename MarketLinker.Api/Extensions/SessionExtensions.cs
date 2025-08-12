7650889d2dedd4cb7e5f83b48b4b722297dddf56
Tiago Catoia <tiagocsz@hotmail.com>
2025-08-11 21:38:33 -0300

    feat: add session extension

---
+++ MarketLinker.Api/Extensions/SessionExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace MarketLinker.Api.Extensions;
+
+public static class SessionExtensions
+{
+    public static IServiceCollection AddAppSession(this IServiceCollection services)
+    {
+        services.AddDistributedMemoryCache();
+        services.AddSession(options =>
+        {
+            options.IdleTimeout = TimeSpan.FromMinutes(30);
+            options.Cookie.HttpOnly = true;
+            options.Cookie.IsEssential = true;
+        });
+        
+        return services;
+    }
+}
