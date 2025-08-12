33594d6f5dcdd3b5c859cdd9cc432a9c6eacb295
Tiago Catoia <tiagocsz@hotmail.com>
2025-08-12 16:26:26 -0300

    feat: add http client extension class

---
+++ MarketLinker.Api/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+
+namespace MarketLinker.Api.Extensions;
+
+public static class HttpClientExtensions
+{
+    public static IServiceCollection AddMarketplaceHttpClients(this IServiceCollection services)
+    {
+        services.AddHttpClient("MercadoLivre", client =>
+        {
+            client.BaseAddress = new Uri("https://api.mercadolibre.com/");
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        });
+        return services;
+    }
+}
